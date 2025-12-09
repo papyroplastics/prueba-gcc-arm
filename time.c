ee4fa7e905fb7263dad2271f58a165d1f89c2ab8
Lucas Llort <lucasllort@gmail.com>
2025-12-09 19:38:55 -0300

    fix test

--- time.c
+++ time.c
@@ -1,14 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <time.h>
 #include "table.h"
 
 int main(void) {
     hash_table_t* table = table_init(1<<16);
     table_insert(table, 192, 581);
 
-    printf("lookup: %lu", table_lookup(table, 192));
-    printf("fixed: %lu", table_lookup_fixed(table, 192));
+    printf("lookup: %u\n", (uint32_t)table_lookup(table, 192));
+    printf("fixed: %u\n", (uint32_t)table_lookup_fixed(table, 192));
 
     return 0;
 }
